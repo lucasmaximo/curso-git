f0e6837ed62a0b02247dff105f8d4e9a7ab50fee
Lucas Máximo <lucasmaximo@outlook.com>
2019-01-06 12:48:15 -0200

    Acrescentada a pergunta sobre como voce esta

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -6,6 +6,7 @@ namespace projeto_teste {
 
             Console.WriteLine("Olá mundo!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?");
 
             Console.ReadLine();
         }
