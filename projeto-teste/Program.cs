f18c81102b7047ad31127e8e0533cef5f7e736b4
Lucas Máximo <lucasmaximo@outlook.com>
2019-01-06 12:24:12 -0200

    Projeto iniciado, mostrando a mensagem Ola mundo

---
+++ projeto-teste/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projeto_teste {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Olá mundo!");
+
+            Console.ReadLine();
+        }
+    }
+}
