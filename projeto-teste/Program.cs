c720dc18a64203bbd9823ef4d63e84fa1946bc4c
Lucas Máximo <lucasmaximo@outlook.com>
2019-01-06 13:54:37 -0200

    Acrescentada a mensagem de despedida

--- projeto-teste/Program.cs
+++ projeto-teste/Program.cs
@@ -7,6 +7,7 @@ namespace projeto_teste {
             Console.WriteLine("Olá mundo!");
             Console.WriteLine("Como você está hoje?");
             Console.WriteLine("Tenha um bom dia!");
+            Console.WriteLine("Tchau");
 
             Console.ReadLine();
         }
